488bcd40eca70da46492dc0851437b45882d3c23
Hamad Ahmad Ansari <hamad.ansarif90@gmail.com>
2025-04-12 03:27:01 +0530

    Add user authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,21 +1,14 @@
 import { Router } from "express";
 
+import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
+
 const authRouter = Router();
 
-//POST request to the sign-up URL
-authRouter.post('/sign-up', (req, res) => {
-  res.send({ title: 'Sign Up Route'});
-});
+// Path: api/v1/auth/:auth-path
 
-//POST request to the sign-in
-authRouter.post('/sign-in', (req, res) => {
-  res.send({ title: 'Sign Up Route'});
-});
-
-//POST request to the sign-out
-authRouter.post('/sign-out', (req, res) => {
-  res.send({ title: 'Sign Up Route'});
-});
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
 
 export default authRouter;
